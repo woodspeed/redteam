b3df727f0f7fe8b245c3ed119307ec9f393aaec7
Viktor Gazdag <woodspeed@gmail.com>
2024-05-22 21:17:17 +0100

    Create license.ps1
    test

---
+++ license.ps1
@@ -0,0 +1,16 @@
+$resourceGroupName = "cvtrsg" # <- Replace with your value
+$location = "europewest" # <- This must be a location that can host Azure Container Instances
+$storageAccountName = "deploymentscript474694" # <- Unique storage account name
+$userManagedIdentity = "scriptRunner" # <- Change this if you want
+
+Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName || New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -Location $location -SkuName Standard_LRS
+
+$uamiObject = New-AzUserAssignedIdentity -ResourceGroupName $resourceGroupName -Name $userManagedIdentity -Location $location
+
+New-AzRoleAssignment -ObjectId $uamiObject.PrincipalId -RoleDefinitionName Reader
+New-AzRoleAssignment -ObjectId $uamiObject.PrincipalId -RoleDefinitionName "Virtual Machine Contributor"
+
+Write-Output "ACIResourceGroup --- $resourceGroupName"
+Write-Output "StorageAccountId --- $((Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName).Id)"
+Write-Output "StorageAccountName --- $storageAccountName"
+Write-Output "RemediationIdentity --- $($uamiObject.Id)"
