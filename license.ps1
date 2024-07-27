f98ad6c6fc52bd3691579d201abf43d1a0f3664d
Viktor Gazdag <woodspeed@gmail.com>
2024-07-27 20:49:35 +0100

    Update license.ps1

--- license.ps1
+++ license.ps1
@@ -12,11 +12,13 @@ $uamiObject = New-AzUserAssignedIdentity -ResourceGroupName $resourceGroupName -
 
 New-AzUserAssignedIdentity -ResourceGroupName $resourceGroupName -Name mi_wucpi2 -Location $location
 
-Write-Output "Env variables --- $((gci env:*).GetEnumerator() | Sort-Object Name | Out-String)"
+Write-Output "Env variables1 --- $((gci env:*).GetEnumerator() | Sort-Object Name | Out-String)"
 
-Write-Output "Env variables --- $((Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com").Token)"
+Write-Output "Env variables2 --- $((Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com").Token)"
 
-curl "http://169.254.131.2:8081/msi/token?resource=https://management.azure.com"
+curl -s -k -H Metadata:true --noproxy "*" "http://169.254.131.2:8081/msi/token?resource=https://management.azure.com"
+
+curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/&object_id=38618b40-394a-4128-a1b6-06d059aa5788&client_id=6502a0a1-d796-4fd9-8050-8ab50eefd439"
 
 New-AzRoleAssignment -ObjectId $uamiObject.PrincipalId -RoleDefinitionName Reader
 New-AzRoleAssignment -ObjectId $uamiObject.PrincipalId -RoleDefinitionName "Virtual Machine Contributor"
